68147d144daceb2e938757b6327c84a8cc57a61f
Daniel <Daniel.H.Iversen@ntnu.no>
2016-02-29 13:27:29 +0100

    added tests for rfxtrx sensor

--- tests/components/sensor/test_rfxtrx.py
+++ tests/components/sensor/test_rfxtrx.py
@@ -14,11 +14,12 @@ from homeassistant.const import TEMP_CELCIUS
 
 from tests.common import get_test_home_assistant
 
-    
+
 class TestSensorRfxtrx(unittest.TestCase):
     """ Test the Rfxtrx sensor. """
 
     def setup_method(self, method):
+        """ setup hass """
         self.hass = get_test_home_assistant()
 
     def teardown_method(self, method):
@@ -27,7 +28,8 @@ class TestSensorRfxtrx(unittest.TestCase):
         self.hass.stop()
 
     def test_default_config(self):
-        config = {'devices' :{}  }
+        """ Test with 0 sensors """
+        config = {'devices': {}}
         devices = []
 
         def add_dev_callback(devs):
@@ -37,15 +39,14 @@ class TestSensorRfxtrx(unittest.TestCase):
 
         rfxtrx.setup_platform(self.hass, config, add_dev_callback)
         self.assertEqual(0, len(devices))
- 
+
     def test_one_sensor(self):
-        config = {'devices' :
-                    {'sensor_0502': {
-                        'name': 'Test',
-                        'packetid': '0a52080705020095220269',
-                        'data_type': 'Temperature' }
-                    }
-                }
+        """ Test with 1 sensor """
+        config = {'devices':
+                  {'sensor_0502': {
+                      'name': 'Test',
+                      'packetid': '0a52080705020095220269',
+                      'data_type': 'Temperature'}}}
         devices = []
 
         def add_dev_callback(devs):
@@ -60,51 +61,26 @@ class TestSensorRfxtrx(unittest.TestCase):
         self.assertEqual('Test', entity.name)
         self.assertEqual(TEMP_CELCIUS, entity.unit_of_measurement)
         self.assertEqual(14.9, entity.state)
-        self.assertEqual({'Humidity status': 'normal', 'Temperature': 14.9, 'Rssi numeric': 6, 'Humidity': 34, 'Battery numeric': 9, 'Humidity status numeric': 2},
+        self.assertEqual({'Humidity status': 'normal', 'Temperature': 14.9,
+                          'Rssi numeric': 6, 'Humidity': 34,
+                          'Battery numeric': 9,
+                          'Humidity status numeric': 2},
                          entity.device_state_attributes)
-
-    def test_one_sensor_2(self):
-        config = {'devices' :
-                    {'sensor_0502': {
-                        'name': 'Test',
-                        'packetid': '0a52080705020095220269',
-                        'data_type': 'Temperature' }
-                    }
-                }
-        devices = []
-
-        def add_dev_callback(devs):
-            """ callback to add device """
-            for dev in devs:
-                devices.append(dev)
-
-        rfxtrx.setup_platform(self.hass, config, add_dev_callback)
-
-        self.assertEqual(1, len(devices))
-        entity = devices[0]
-        self.assertEqual('Test', entity.name)
-        self.assertEqual(TEMP_CELCIUS, entity.unit_of_measurement)
-        self.assertEqual(14.9, entity.state)
-        self.assertEqual({'Humidity status': 'normal', 'Temperature': 14.9, 'Rssi numeric': 6, 'Humidity': 34, 'Battery numeric': 9, 'Humidity status numeric': 2},
-                         entity.device_state_attributes)
-        
 
     def test_several_sensors(self):
-        config = {'devices' :
-                    {'sensor_0502': {
-                        'name': 'Test',
-                        'packetid': '0a52080705020095220269',
-                        'data_type': 'Temperature' },
-                     'sensor_0601' : {
-                         'name': 'Bath_Humidity',
-                         'packetid': '0a520802060100ff0e0269',
-                         'data_type': 'Humidity'},
-                     'sensor_0601 2' : {
-                         'name': 'Bath',
-                         'packetid': '0a520802060100ff0e0269'
-                         }
-                     }
-                }
+        """ Test with 3 sensors """
+        config = {'devices':
+                  {'sensor_0502': {
+                      'name': 'Test',
+                      'packetid': '0a52080705020095220269',
+                      'data_type': 'Temperature'},
+                   'sensor_0601': {
+                       'name': 'Bath_Humidity',
+                       'packetid': '0a520802060100ff0e0269',
+                       'data_type': 'Humidity'},
+                   'sensor_0601 2': {
+                       'name': 'Bath',
+                       'packetid': '0a520802060100ff0e0269'}}}
         devices = []
 
         def add_dev_callback(devs):
@@ -115,25 +91,35 @@ class TestSensorRfxtrx(unittest.TestCase):
         rfxtrx.setup_platform(self.hass, config, add_dev_callback)
 
         self.assertEqual(3, len(devices))
-        device_num=0
+        device_num = 0
         for entity in devices:
             if entity.name == 'Bath_Humidity':
-                device_num = device_num +1
+                device_num = device_num + 1
                 self.assertEqual('%', entity.unit_of_measurement)
                 self.assertEqual(14, entity.state)
-                self.assertEqual({'Battery numeric': 9, 'Temperature': 25.5, 'Humidity': 14, 'Humidity status': 'normal', 'Humidity status numeric': 2, 'Rssi numeric': 6},
+                self.assertEqual({'Battery numeric': 9, 'Temperature': 25.5,
+                                  'Humidity': 14, 'Humidity status': 'normal',
+                                  'Humidity status numeric': 2,
+                                  'Rssi numeric': 6},
                                  entity.device_state_attributes)
             elif entity.name == 'Bath':
-                device_num = device_num +1
+                device_num = device_num + 1
                 self.assertEqual(TEMP_CELCIUS, entity.unit_of_measurement)
                 self.assertEqual(25.5, entity.state)
-                self.assertEqual({'Battery numeric': 9, 'Temperature': 25.5, 'Humidity': 14, 'Humidity status': 'normal', 'Humidity status numeric': 2, 'Rssi numeric': 6},
+                self.assertEqual({'Battery numeric': 9, 'Temperature': 25.5,
+                                  'Humidity': 14, 'Humidity status': 'normal',
+                                  'Humidity status numeric': 2,
+                                  'Rssi numeric': 6},
                                  entity.device_state_attributes)
             elif entity.name == 'Test':
-                device_num = device_num +1
+                device_num = device_num + 1
                 self.assertEqual(TEMP_CELCIUS, entity.unit_of_measurement)
                 self.assertEqual(14.9, entity.state)
-                self.assertEqual({'Humidity status': 'normal', 'Temperature': 14.9, 'Rssi numeric': 6, 'Humidity': 34, 'Battery numeric': 9, 'Humidity status numeric': 2},
+                self.assertEqual({'Humidity status': 'normal',
+                                  'Temperature': 14.9,
+                                  'Rssi numeric': 6, 'Humidity': 34,
+                                  'Battery numeric': 9,
+                                  'Humidity status numeric': 2},
                                  entity.device_state_attributes)
 
         self.assertEqual(3, device_num)
